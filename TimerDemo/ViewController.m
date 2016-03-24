7b0d31591d6583c542500744108c54c9e96a21d9
yangyonghui <yangyonghui@exutech.cn>
2016-03-24 15:12:02 +0800

    高频降低为低频
    我们在高频的坐着重复动作，高频动作如果是网络请求，有的服务器会受不了，如何将这些高频动作合理的展示出来呢

--- TimerDemo/ViewController.m
+++ TimerDemo/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController (){
+    NSInteger index;
+    NSInteger tapIndex;
+    NSInteger tapIndex2;
+    BOOL excuteFinished;
+}
 
 @end
 
@@ -16,7 +21,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    index = 0;
+    tapIndex = 0;
+    excuteFinished = YES;
+    UIButton *button1 = [[UIButton alloc] init];
+    button1.frame = CGRectMake(40, 50, 50, 50);
+    button1.backgroundColor = [UIColor redColor];
+    [button1 addTarget:self action:@selector(buttonAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button1];
+    
+}
+
+- (void)buttonAction:(UIButton *)sender {
+    tapIndex++;
+    tapIndex2++;
+    if (excuteFinished) {//里面没有执行完毕我就不允许你进去，多好的东西。
+        excuteFinished = NO;
+        dispatch_time_t time = dispatch_time(DISPATCH_TIME_NOW, 1* NSEC_PER_SEC);
+        dispatch_after(time, dispatch_get_main_queue(), ^{
+            NSLog(@"里面执开始执行啦");
+            index ++;
+            NSLog(@"执行的数量%ld",(long)index);
+            NSLog(@"tapIndex执行的数量%ld",(long)tapIndex);
+            tapIndex = 0;
+            excuteFinished = YES;
+            
+            
+        });
+        
+    }
+    NSLog(@"一共点击了多少次%ld",(long)tapIndex2);
+   
 }
 
 - (void)didReceiveMemoryWarning {
